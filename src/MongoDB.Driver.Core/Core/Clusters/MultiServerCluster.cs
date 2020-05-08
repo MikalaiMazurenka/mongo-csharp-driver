f56890021f29e1c558c4bdddf3e114d8643e1685
Mikalai Mazurenka <mikalai.mazurenka@mongodb.com>
2020-05-08 15:27:05 +0300

    CSHARP-2912: Clarify behavior when 'me' field doesn't match any values in 'hosts' array

--- src/MongoDB.Driver.Core/Core/Clusters/MultiServerCluster.cs
+++ src/MongoDB.Driver.Core/Core/Clusters/MultiServerCluster.cs
@@ -358,7 +358,8 @@ namespace MongoDB.Driver.Core.Clusters
             clusterDescription = EnsureServers(clusterDescription, args.NewServerDescription, newServers);
 
             if (args.NewServerDescription.CanonicalEndPoint != null &&
-                !EndPointHelper.Equals(args.NewServerDescription.CanonicalEndPoint, args.NewServerDescription.EndPoint))
+                !EndPointHelper.Equals(args.NewServerDescription.CanonicalEndPoint, args.NewServerDescription.EndPoint) &&
+                args.NewServerDescription.Type != ServerType.ReplicaSetPrimary)
             {
                 return RemoveServer(clusterDescription, args.NewServerDescription.EndPoint, "CanonicalEndPoint is different than seed list EndPoint.");
             }
